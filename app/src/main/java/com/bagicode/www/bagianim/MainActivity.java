344f7b8ffef9b27b62bdb03b0d8658ae54b6e403
Bagicode <bagicode@gmail.com>
2017-08-11 21:37:11 +0700

    Initial commit

---
+++ app/src/main/java/com/bagicode/www/bagianim/MainActivity.java
@@ -0,0 +1,24 @@
+package com.bagicode.www.bagianim;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void zoom(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.clockwise);
+        image.startAnimation(animation1);
+    }
+}
